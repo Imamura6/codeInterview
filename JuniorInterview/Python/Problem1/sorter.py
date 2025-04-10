860a6e1a6982113ebfeb3606027efa4592a2ef39
IMAMURA Vitor <vitor.imamura@thalesgroup.com>
2025-04-10 11:44:56 -0300

    update python sorter test

--- JuniorInterview/Python/Problem1/sorter.py
+++ JuniorInterview/Python/Problem1/sorter.py
@@ -15,8 +15,8 @@ class BubbleSorter(Sorter):
         n = len(data)
         for i in range(n - 1):
             for j in range(n - i - 1):
-                if data[j] > data[j + 1]
-                data[j], data[j + 1] = data[j], data[j]
+                if data[j] > data[j]:
+                    data[j], data[j + 1] = data[j + 1], data[j]
 
 
 class SelectionSorter(Sorter):
@@ -26,8 +26,8 @@ class SelectionSorter(Sorter):
         for i in range(n - 1):
             min_idx = i
             for j in range(i + 1, n):
-                if data[j] > data[min_idx]:
-                    min_idx = j
+                if data[j] > data[min_idx]
+                min_idx = j
             data[i], data[min_idx] = data[min_idx], data[i]
 
 
